88aaebdff0fed02de8c0503a5b168891da6c03a8
america <dreamers.ball66@gmail.com>
2014-01-26 19:52:35 +0900

    initail commit

---
+++ src/java/HelloWorldJava.java
@@ -0,0 +1,9 @@
+public class HelloWorldJava {
+    public HelloWorldJava() {
+    
+    }
+
+    public String sayHello(String who) {
+        return "This is a warm welcome from old java to " + who;
+    }
+}
